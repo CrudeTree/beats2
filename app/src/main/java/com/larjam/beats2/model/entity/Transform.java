2d669f67fbbfb1aaf74cefa04af5638bf4348029
James Larivee <mr_15th_entertainer@yahoo.com>
2019-10-13 21:12:40 -0600

    Added Audio & Transform class + room annotations

---
+++ app/src/main/java/com/larjam/beats2/model/entity/Transform.java
@@ -0,0 +1,17 @@
+package com.larjam.beats2.model.entity;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(
+
+
+)
+
+public class Transform {
+
+  @PrimaryKey(autoGenerate = true)
+  @ColumnInfo(name = "Transformed_id")
+
+}
