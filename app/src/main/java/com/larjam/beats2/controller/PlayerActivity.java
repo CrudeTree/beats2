564fd66daa97ad452a6f5aa4ad86d599e1bc93f5
James Larivee <mr_15th_entertainer@yahoo.com>
2019-11-15 19:10:49 -0700

    Working on pitch shift. Some errors.

--- app/src/main/java/com/larjam/beats2/controller/PlayerActivity.java
+++ app/src/main/java/com/larjam/beats2/controller/PlayerActivity.java
@@ -17,16 +17,32 @@ import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import androidx.lifecycle.ViewModelProviders;
+import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.AudioProcessor;
+import be.tarsos.dsp.PitchShifter;
+import be.tarsos.dsp.io.android.AndroidFFMPEGLocator;
+import be.tarsos.dsp.io.android.AudioDispatcherFactory;
+
+import be.tarsos.dsp.pitch.PitchDetectionHandler;
+import be.tarsos.dsp.pitch.PitchDetectionResult;
+import be.tarsos.dsp.pitch.PitchProcessor;
+import be.tarsos.dsp.pitch.PitchProcessor.PitchEstimationAlgorithm;
+
+import be.tarsos.dsp.resample.RateTransposer;
+import be.tarsos.dsp.resample.Resampler;
 import com.larjam.beats2.GoogleSignInService;
 import com.larjam.beats2.R;
 import com.larjam.beats2.viewmodel.MainViewModel;
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
+import java.nio.Buffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -47,7 +63,13 @@ public class PlayerActivity extends AppCompatActivity {
   private ArrayList<String> arrayList;
   private ListView listView;
   private int songIndex;
+  private PitchShifter pitchShifter;
+  private float[] buffer;
+  private AudioDispatcher dispatcher;
 
+
+  static {
+  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -73,6 +95,8 @@ public class PlayerActivity extends AppCompatActivity {
       uri = Uri.parse("/storage/0EBB-01CB/MySongList/Pumped up Kicks- Foster The People.mp3");
 
     }
+
+    startFile(new File(String.valueOf(uri)));
 
     songIndex = arrayList.indexOf(uri.toString());
 
@@ -124,6 +148,93 @@ public class PlayerActivity extends AppCompatActivity {
     }
 
   }
+
+
+
+
+
+
+
+
+  private void startFile(File file) {
+    int currentFactor = 10;
+    final int size = 2048;
+    final int overlap = 2048 - 128;
+    int samplerate = 44100;
+    new AndroidFFMPEGLocator(this);
+    final AudioDispatcher d = AudioDispatcherFactory.fromPipe(file.getAbsolutePath(), samplerate, size, overlap);
+    pitchShifter = new PitchShifter(1.0/currentFactor, samplerate, size, overlap);
+
+    d.addAudioProcessor(new AudioProcessor() {
+      @Override
+      public void processingFinished() {
+        // TODO Auto-generated method stub
+
+      }
+
+      @Override
+      public boolean process(AudioEvent audioEvent) {
+        buffer = audioEvent.getFloatBuffer();
+        return true;
+      }
+    });
+
+    d.addAudioProcessor(pitchShifter);
+
+    d.addAudioProcessor(new AudioProcessor() {
+      Resampler r= new Resampler(false,0.1,4.0);
+      @Override
+      public void processingFinished() {
+      }
+
+      @Override
+      public boolean process(AudioEvent audioEvent) {
+
+        float factor = (float) (currentFactor);
+        float[] src = audioEvent.getFloatBuffer();
+        float[] out = new float[(int) ((size-overlap) * factor)];
+        r.process(factor, src, overlap,size-overlap, false, out, 0, out.length);
+        //The size of the output buffer changes (according to factor).
+        d.setStepSizeAndOverlap(out.length, 0);
+
+        audioEvent.setFloatBuffer(out);
+        audioEvent.setOverlap(0);
+
+        return true;
+      }
+    });
+//    d.addAudioProcessor(rateTransposer);
+//    try {
+//      d.addAudioProcessor(new AudioPlayer(d.getFormat()));
+//    } catch (LineUnavailableException e) {
+//      e.printStackTrace();
+//    }
+    d.addAudioProcessor(new AudioProcessor() {
+
+      @Override
+      public void processingFinished() {
+      }
+
+      @Override
+      public boolean process(AudioEvent audioEvent) {
+        d.setStepSizeAndOverlap(size, overlap);
+        d.setAudioFloatBuffer(buffer);
+        audioEvent.setFloatBuffer(buffer);
+        audioEvent.setOverlap(overlap);
+        return true;
+      }
+    });
+    dispatcher = d ;
+    new Thread(d).start();
+
+  }
+
+
+
+
+
+
+
 
   private void changeSeekbar() {
     if (player != null && player.isPlaying()) {
